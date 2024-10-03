58d9f5e521abb8aa45b0a73650d35e304e350c85
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-03 12:40:43 +0700

    add: search feature

--- list-makanan/src/App.jsx
+++ list-makanan/src/App.jsx
@@ -2,13 +2,23 @@ import './App.css'
 import Search from './components/Search'
 import Content from './components/Content'
 
-import menu from './data/menu.json'
+import menus from './data/menu.json'
+import { useState } from 'react'
 
 function App() {
 
+  const [menu, setMenu] = useState(menus)
+
+  const cari = (val) => {
+    const filteredMenu = menus.filter((m) => m.name.includes(val))
+    setMenu(filteredMenu)
+
+  }
+
+
   return (
     <>
-      <Search/>
+      <Search nyari={cari} />
       {
         menu.map((props, index) => <Content key={index} {...props} />)
       }
--- list-makanan/src/components/Search.jsx
+++ list-makanan/src/components/Search.jsx
@@ -1,9 +1,21 @@
-function Search(){
+/* eslint-disable react/prop-types */
+
+function Search(props){
+
+    // const [search, setSearch] = useState("")
+    // const mencari = (e) => {
+    //     console.log(e.target.value)
+    //     setSearch(e.target.value)
+    // }
+
+    const mencari = (e) => {
+        props.nyari(e.target.value)
+    }
 
     return (
         <>
             <h2>Search</h2>
-            <input type="text" />
+            <input onChange={mencari} type="text" />
         </>
     )
 }
