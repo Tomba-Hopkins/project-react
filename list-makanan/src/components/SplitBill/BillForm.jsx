415008364da22994a3bffed10b274af2dc84ea79
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-12 10:21:52 +0700

    add: html dari bill patungan form

--- list-makanan/src/components/SplitBill/BillForm.jsx
+++ list-makanan/src/components/SplitBill/BillForm.jsx
@@ -1,5 +1,29 @@
 function BillForm(){
-    return
+    return (
+        <main>
+            <h2>Patungan Bareng</h2>
+            <form>
+                <label htmlFor="">Total Pengeluaran</label>
+                <input type="text" />
+
+
+                <label htmlFor="">Pengeluaran mu</label>
+                <input type="text" />
+
+                <label htmlFor="">Pengeluaran name</label>
+                <input type="text" />
+
+
+                <label htmlFor="">Ditolong oleh</label>
+                <select>
+                    <option value="kamu">kamu</option>
+                </select>
+
+                <button>Submit</button>
+
+            </form>
+        </main>
+    )
 }
 
 export default BillForm
