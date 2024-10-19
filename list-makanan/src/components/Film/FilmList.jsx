b59093c385f426ebf1c23bdd537cde3da8b99dde
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-19 09:25:38 +0700

    add: styling box movies

--- list-makanan/src/components/Film/FilmList.jsx
+++ list-makanan/src/components/Film/FilmList.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
+
+import './film.css'
+
 export default function FilmList({listFilm}){
     console.log(listFilm)
     return (
         <>
             {listFilm.map((film) => {
-            return <div style={{
-                margin: '2rem auto'
-            }}
+            return <div className="box"
             key={film.imdbID}>
 
-                    <h2>{film.Title}</h2>
+                    <h3>{film.Title}</h3>
                     <img src={film.Poster} alt={film.Title} />
                     <p>{film.Year}</p>
-                    <hr style={{border: '1px solid white'}} />
 
 
                 </div>
