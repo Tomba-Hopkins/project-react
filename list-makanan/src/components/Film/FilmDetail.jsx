803a784acf268704a43638da2f02ee4e39c8d67f
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-20 15:59:43 +0700

    add: loading component

--- list-makanan/src/components/Film/FilmDetail.jsx
+++ list-makanan/src/components/Film/FilmDetail.jsx
@@ -1,21 +1,45 @@
 import { useEffect, useState } from "react"
+import Loading from '../Loading'
 
 /* eslint-disable react/prop-types */
 export default function FilmDetail({handleDetailActive, pilihIdFilm, film_api}){
 
     const [filmDetail, setFilmDetail] = useState()
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMsg, setErrorMsg] = useState('')
     
 
     useEffect(() => {
         const fetchDetailFilm = async () => {
-            const res = await fetch(`http://www.omdbapi.com/?apikey=${film_api}&i=${pilihIdFilm}`)
-            const data = await res.json()
-            setFilmDetail(data)
+            try {
+                setIsLoading(true)
+                const res = await fetch(`http://www.omdbapi.com/?apikey=${film_api}&i=${pilihIdFilm}`)
+
+                if(!res.ok) throw new Error('Something went wrong')
+                
+                const data = await res.json()
+
+                if(data.Response === "False") throw new Error('Error')
+                
+                setFilmDetail(data)
+            } catch(err) {
+                console.log(err)
+                setErrorMsg(err.message)
+            } finally {
+                setIsLoading(false)
+            }
         }
 
         fetchDetailFilm()
         
     }, [film_api, pilihIdFilm])
+
+
+    if(isLoading) return <Loading/>
+    if(errorMsg) return <p>{errorMsg}</p>
+    if(!filmDetail) return <Loading/>
+    
+    
 
     const {
         Actors: pemain,
@@ -29,12 +53,12 @@ export default function FilmDetail({handleDetailActive, pilihIdFilm, film_api}){
         Director: sutradara
     } = filmDetail
     
-    console.log(filmDetail)
+
 
     
     return (
         <>
-            <button onClick={handleDetailActive}>❌</button>
+           <button onClick={handleDetailActive}>❌</button>
             <h2>{judul}</h2>
             <img src={gambar} alt={judul} />
             <p>{rilis}</p>
---
+++ list-makanan/src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import '../index.css'
+
+export default function Loading(){
+    return (
+        <>
+            <div className="loading"></div>
+            <p><small>Loading.....</small></p>
+        </>
+    )
+}
