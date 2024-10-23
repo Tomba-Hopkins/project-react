ef4fc84ac9bae6d1b759882c058cfca2faef1eab
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-23 12:32:51 +0700

    add: average rating film

--- list-makanan/src/components/Film/FilmCollection.jsx
+++ list-makanan/src/components/Film/FilmCollection.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
+
 /* eslint-disable react/prop-types */
 export default function FilmCollection({koleksiFilm}){
+
+    const [avgRating, setRating] = useState(0)
+
+    useEffect(() => {
+
+        if(koleksiFilm.length > 0) {
+            const sum = koleksiFilm.reduce((acc, film) => acc + film.Rating, 0)
+            const avg = sum / koleksiFilm.length
+            setRating(avg)
+        }
+
+    }, [koleksiFilm])
+    
+    
+    
     return (
         <>
             <section>
                 <h2>M Y Collection 👺</h2>
-                {koleksiFilm.map((film, index) => {
+                <h3>Summary</h3>
+                <p>⭐ {avgRating} | ⌛</p>
+
+                {koleksiFilm.map((film, index) => {                    
                     return (
                         <div key={index} className="koleksi">
                             <p>{index + 1}</p>
