a9fb0d1da6eebe9cf633882ff2cd6fa637f056e4
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-06 11:00:45 +0700

    add: typewriter

--- portfolioku/src/App.jsx
+++ portfolioku/src/App.jsx
@@ -1,20 +1,47 @@
 import { motion } from "motion/react";
+import TypewriterComponent from "typewriter-effect";
 
 function App() {
   return (
     <div className="bg-slate-900 text-slate-100 min-h-screen flex flex-col items-center">
-      <motion.header
-        className="h-screen w-full p-4 flex gap-8 justify-center items-center"
-        initial={{ y: -100 }}
-        animate={{ y: 0 }}
-        transition={{ duration: 1.2, stiffness: 50 }}
-      >
-        <motion.p className="text-5xl">Hello, im Smith i am a</motion.p>
-        <img
-          className="w-24 h-24 rounded-full"
-          src="../public/img/me.jpg"
-          alt="me.jpg"
-        />
+      <motion.header className="h-screen w-full p-4 flex flex-col gap-8 justify-center items-center">
+        <div className="flex items-center justify-center gap-8">
+          <motion.p
+            className="text-5xl"
+            initial={{ y: -300 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1.2, stiffness: 50 }}
+          >
+            Hello, i am smith
+          </motion.p>
+          <motion.img
+            className="w-24 h-24 rounded-full"
+            src="/img/me.jpg"
+            alt="me.jpg"
+            initial={{ x: 300 }}
+            animate={{ x: 0 }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.7 }}
+            transition={{ duration: 0.9, stiffness: 50 }}
+          />
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <motion.p className="text-2xl flex gap-4">I am a</motion.p>
+          <span className="text-green-400 text-2xl">
+            <TypewriterComponent
+              options={{
+                strings: [
+                  "Web developer",
+                  "Cyber Security Enthusiast",
+                  "Graphic Designer",
+                ],
+                autoStart: true,
+                loop: true,
+                cursor: "_",
+              }}
+            />
+          </span>
+        </div>
       </motion.header>
     </div>
   );
