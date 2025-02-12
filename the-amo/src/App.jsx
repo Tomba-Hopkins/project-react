686bfde49d0e216df35a207534b18d963f1ec083
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-02-12 17:41:02 +0700

    add: page 1

--- the-amo/src/App.jsx
+++ the-amo/src/App.jsx
@@ -6,7 +6,7 @@ import { Second } from './components/Second'
 import { motion } from 'motion/react'
 
 
-// const words = ["beautiful"]
+const words = ["beautiful"]
 const pages = [First, Second]
 
 
@@ -20,6 +20,14 @@ function App() {
   const pageHandler = () => {
     if (page != pages.length - 1) {
       setPage((p) => p + 1)
+      setCorrect(false)
+    }
+  }
+
+
+  const submitHandler = ans => {
+    if (ans == words[page]) {
+      setCorrect(true)
     }
   }
 
@@ -27,11 +35,10 @@ function App() {
   return (
     <>
       <main className='min-h-screen flex flex-col justify-center text-center items-center bg-slate-800 text-pink-500 gap-6 font-semibold font-flower tracking-widest'>
-        <NowPage pageHandler={pageHandler} setCorrect={setCorrect} />
+        <NowPage correct={correct} submitHandler={submitHandler} pageHandler={pageHandler} setCorrect={setCorrect} />
         {
           correct && page < pages.length - 1 && (
             <motion.button animate={{ scale: [1, 1.5, 1] }} transition={{ repeat: Infinity, ease: 'easeIn', duration: 2 }} className='px-6 py-1 bg-slate-300 rounded-sm rounded-tl-none rounded-tr-none active:animate-ping active:border-2 border-pink-500' onClick={pageHandler}>💗</motion.button>
-
           )
         }
       </main>
--- the-amo/src/components/First.jsx
+++ the-amo/src/components/First.jsx
@@ -1,24 +1,45 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
 
 
-export function First() {
+// eslint-disable-next-line react/prop-types
+export function First({ submitHandler, correct }) {
+
+    const [type, SetType] = useState("")
+
+
+    const inputHandler = (e) => {
+        SetType(e.target.value)
+    }
+
+
     return (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="overflow-x-hidden flex flex-col gap-4">
             <h1 className="text-slate-100 py-10 my-10">Welcome again <motion.span className="font-extrabold text-pink-600" initial={{ marginLeft: 50 }} animate={{ marginLeft: 0 }} transition={{ duration: 1 }} >My Love</motion.span> </h1>
 
 
-            <motion.p className="my-10" initial={{ scale: 2, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ delay: 1, duration: 0.7, ease: 'easeIn' }}>I have some game for you <span className="text-pink-200">sweetie</span> </motion.p>
+            <AnimatePresence>
+                {!correct && (
 
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2 }} className="my-10">
-                <motion.p animate={{ opacity: [1, 0, 1] }} transition={{ duration: 1, repeat: Infinity, ease: 'backIn' }}>Decode this base64 first okay</motion.p>
+                    <motion.div exit={{ opacity: 0, scale: 0 }}>
+                        <motion.p className="my-10" initial={{ scale: 2, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ delay: 1, duration: 0.7, ease: 'easeIn' }}>I have some game for you <span className="text-pink-200">sweetie</span> </motion.p>
 
-                <motion.p className="font-mono text-slate-50 text-xs my-4 p-2 bg-pink-500" animate={{ borderRadius: [40, 0, 40] }} transition={{ repeat: Infinity, duration: 5, ease: 'circInOut' }}>YmVhdXRpZnVs</motion.p>
-            </motion.div>
+                        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2 }} className="my-10">
+                            <motion.p animate={{ opacity: [1, 0, 1] }} transition={{ duration: 1, repeat: Infinity, ease: 'backIn' }}>Decode this base64 first okay</motion.p>
 
-            <motion.div className="w-full my-10 flex gap-2 p-2">
-                <input className="bg-slate-300 p-2 text-slate-900" type="text" />
-                <button className="bg-pink-500 text-slate-100 p-2">submit</button>
-            </motion.div>
+                            <motion.p className="font-mono text-slate-50 text-xs my-4 p-2 bg-pink-500" animate={{ borderRadius: [40, 0, 40] }} transition={{ repeat: Infinity, duration: 5, ease: 'circInOut' }}>YmVhdXRpZnVs</motion.p>
+                        </motion.div>
+
+                        <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2.4 }}>Decode in <a href="https://www.base64decode.org/" target="_blank" className="p-1 py-0 bg-pink-500 text-slate-900">Here</a></motion.p>
+
+                        <motion.div initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.7 }} className=" my-10 overflow-hidden flex gap-2 p-2">
+                            <input value={type} onChange={inputHandler} placeholder="Submit in here" className="bg-slate-300 p-2 text-slate-900" type="text" />
+                            <button onClick={submitHandler(type)} className="bg-pink-500 text-slate-100 p-2">submit</button>
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+
 
 
 
