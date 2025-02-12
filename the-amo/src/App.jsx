b5f536f7159eb44e99e1862b0f4cec45d87b75b0
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-02-12 16:06:05 +0700

    add: dikit

--- the-amo/src/App.jsx
+++ the-amo/src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from 'react'
 import './styles.css'
 import { First } from './components/First'
 import { Second } from './components/Second'
+import { motion } from 'motion/react'
 
 
+// const words = ["beautiful"]
 const pages = [First, Second]
 
 
 function App() {
 
   const [page, setPage] = useState(0)
+  const [correct, setCorrect] = useState(false)
   const NowPage = pages[page]
 
 
@@ -24,10 +27,10 @@ function App() {
   return (
     <>
       <main className='min-h-screen flex flex-col justify-center text-center items-center bg-slate-800 text-pink-500 gap-6 font-semibold font-flower tracking-widest'>
-        <NowPage pageHandler={pageHandler} />
+        <NowPage pageHandler={pageHandler} setCorrect={setCorrect} />
         {
-          page < pages.length - 1 && (
-            <button className='px-6 py-1 bg-slate-300 rounded-sm rounded-tl-none rounded-tr-none active:animate-ping active:border-2 border-pink-500' onClick={pageHandler}>💗</button>
+          correct && page < pages.length - 1 && (
+            <motion.button animate={{ scale: [1, 1.5, 1] }} transition={{ repeat: Infinity, ease: 'easeIn', duration: 2 }} className='px-6 py-1 bg-slate-300 rounded-sm rounded-tl-none rounded-tr-none active:animate-ping active:border-2 border-pink-500' onClick={pageHandler}>💗</motion.button>
 
           )
         }
--- the-amo/src/components/First.jsx
+++ the-amo/src/components/First.jsx
@@ -3,8 +3,25 @@ import { motion } from "motion/react";
 
 export function First() {
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="w-full h-full">
-            <h1 className="text-slate-100 py-10">Welcome again <motion.span className="font-extrabold text-pink-600" initial={{ marginLeft: 50 }} animate={{ marginLeft: 0 }} transition={{ duration: 1 }} >My Love</motion.span> </h1>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="overflow-x-hidden flex flex-col gap-4">
+            <h1 className="text-slate-100 py-10 my-10">Welcome again <motion.span className="font-extrabold text-pink-600" initial={{ marginLeft: 50 }} animate={{ marginLeft: 0 }} transition={{ duration: 1 }} >My Love</motion.span> </h1>
+
+
+            <motion.p className="my-10" initial={{ scale: 2, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ delay: 1, duration: 0.7, ease: 'easeIn' }}>I have some game for you <span className="text-pink-200">sweetie</span> </motion.p>
+
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2 }} className="my-10">
+                <motion.p animate={{ opacity: [1, 0, 1] }} transition={{ duration: 1, repeat: Infinity, ease: 'backIn' }}>Decode this base64 first okay</motion.p>
+
+                <motion.p className="font-mono text-slate-50 text-xs my-4 p-2 bg-pink-500" animate={{ borderRadius: [40, 0, 40] }} transition={{ repeat: Infinity, duration: 5, ease: 'circInOut' }}>YmVhdXRpZnVs</motion.p>
+            </motion.div>
+
+            <motion.div className="w-full my-10 flex gap-2 p-2">
+                <input className="bg-slate-300 p-2 text-slate-900" type="text" />
+                <button className="bg-pink-500 text-slate-100 p-2">submit</button>
+            </motion.div>
+
+
+
         </motion.div>
     )
 }
